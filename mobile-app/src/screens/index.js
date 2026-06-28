bde0f3f3e7c608270e7eeb763111d57b526f343e
iluvsunset <239751831+iluvsunset@users.noreply.github.com>
2026-06-28 15:23:11 +0200

    chore: Remove unused iOS and mobile app scaffolding

--- mobile-app/src/screens/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './Home';
-export { default as Calendar } from './Calendar';
-export { default as Add } from './Add';
-export { default as Profile } from './Profile';
-export { default as Login } from './Login';
