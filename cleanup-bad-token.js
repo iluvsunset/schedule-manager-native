adbd1f7da1fe8980800e87e551b525c43bc5eb86
iluvsunset <239751831+iluvsunset@users.noreply.github.com>
2026-07-08 13:32:34 +0200

    chore: remove debug scripts

--- cleanup-bad-token.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { admin } = require('./server/_utils');
-const db = admin.firestore();
-
-async function run() {
-  await db.collection('gcal_tokens').doc('PL0gz7PfSbhaYxZVYdcAh0e7gS72').delete();
-  console.log('Deleted bad token');
-}
-run();
